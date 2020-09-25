cf5af4387c0fd9deafe72e047148d2eea248b383
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-09-25 18:41:25 +0000

    refactored to be more OO

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// // Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    logState: function () {
+      const title = this.title;
+      const complete = this.complete;
+      console.log(`${title} has${complete ? " " : " not "}been completed`);
+    },
+    completeTask: function() {
+      this.complete = true;
+    }
+  };
+  return task;
 }
-
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+function completeTask(task) {
+  task.complete = true;
 }
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+function logTaskState(task) {
+
 }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+let cleanCatLitter = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
+let doLaundry = newTask("Do Laundry", "😨"); // task 1
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+cleanCatLitter.logState(); // Clean Cat Litter has not been completed
+cleanCatLitter.completeTask();
+cleanCatLitter.logState(); // Clean Cat Litter has been completed
